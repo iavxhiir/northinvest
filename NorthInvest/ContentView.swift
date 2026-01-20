5644063141ce686390637054c6129473dd77132d
Irdi Avxhi <irdi.avxhi@icloud.com>
2026-01-20 21:19:03 +0100

    Initial Commit

---
+++ NorthInvest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NorthInvest
+//
+//  Created by Irdi Avxhi on 20.1.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NorthInvest/NorthInvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NorthInvestApp.swift
+//  NorthInvest
+//
+//  Created by Irdi Avxhi on 20.1.26.
+//
+
+import SwiftUI
+
+@main
+struct NorthInvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
